6ecd56a3f726395b612139dea41fc699385fab23
1750315928@qq.com <1750315928@qq.com>
2021-01-25 10:46:09 +0800

    no message

--- vs1/vs1/main.cpp
+++ vs1/vs1/main.cpp
@@ -1,9 +1,5 @@
-
 #include "add.h"
 int main()
 {
-	printf("hello world\n");
-	printf("%d\n", add(2, 3));
-	printf("hello world\n");
-	getchar();
+	return 0;
 }
---
+++ vs1/vs2/main.cpp
@@ -0,0 +1,10 @@
+#include "add.h"
+//#include <stdlib.h>
+int main()
+{
+	printf("%d\n", add(3,4));
+	//system("pause");
+	getchar();
+	return 0;
+
+}
